a53f3fb67ce0fee6b7fbcb7067d07b0c05be1d91
CHuynh97 <chris.huynh17@gmail.com>
2019-02-04 18:31:53 -0500

    finished lab 1 part 1.

--- lab1/baseball.sql
+++ lab1/baseball.sql
@@ -48,17 +48,62 @@ LOAD DATA LOCAL INFILE 'Fielding.csv'
     (playerID, yearID, stint, teamID, lgID, POS, G, GS, InnOuts, PO, A, E, DP, PB, WP, SB, CS, ZR);
 
 -- 3)
+ALTER TABLE AllStarfull ADD PRIMARY KEY (playerID, gameID);
+ALTER TABLE AllStarfull ADD CONSTRAINT fk_AllStarfull_Teams FOREIGN KEY (teamID) REFERENCES Teams(teamID);
+ALTER TABLE AllStarfull ADD CONSTRAINT fk_AllStarfull_Master FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE Appearances ADD PRIMARY KEY (playerID, yearID, teamID);
+ALTER TABLE Appearances ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
+ALTER TABLE Appearances ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE AwardsManagers ADD PRIMARY KEY (playerID, awardID, yearID);
+ALTER TABLE AwardsManagers ADD FOREIGN KEY (playerID) REFERENCES managers(playerID);
+ALTER TABLE AwardsManagers ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE AwardsPlayers ADD PRIMARY KEY (playerID, awardID, yearID, lgID);
+ALTER TABLE AwardsPlayers ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE AwardsShareManagers ADD PRIMARY KEY (playerID, yearID, awardID);
+ALTER TABLE AwardsShareManagers ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+ALTER TABLE AwardsShareManagers ADD FOREIGN KEY (playerID) REFERENCES managers(playerID);
+
+ALTER TABLE AwardsSharePlayers ADD PRIMARY KEY (playerID, yearID, awardId);
+ALTER TABLE AwardsSharePlayers ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE Batting ADD PRIMARY KEY (playerID, yearID, stint);
+ALTER TABLE Batting ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+ALTER TABLE Batting ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
+
+ALTER TABLE BattingPost ADD PRIMARY KEY (playerID, yearID, round);
+ALTER TABLE BattingPost ADD CONSTRAINT fk_BattingPost_Teams FOREIGN KEY (teamID) REFERENCES Teams(teamID);
+ALTER TABLE BattingPost ADD CONSTRAINT fk_BattingPost_Master FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE CollegePlaying ADD PRIMARY KEY (playerID, schoolID, yearID);
+ALTER TABLE CollegePlaying ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+DELETE FROM CollegePlaying WHERE schoolID NOT IN (SELECT schoolID FROM schools);
+ALTER TABLE CollegePlaying ADD FOREIGN KEY (schoolID) REFERENCES schools(schoolID);
+
+ALTER TABLE Fielding ADD PRIMARY KEY (playerID, yearID, stint, POS);
+ALTER TABLE Fielding ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+ALTER TABLE Fielding ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
+
+ALTER TABLE FieldingOF ADD PRIMARY KEY (playerID, yearID, stint);
+ALTER TABLE FieldingOF ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+
+ALTER TABLE FieldingOFSplit ADD PRIMARY KEY (playerID, yearID, pos, stint);
+ALTER TABLE FieldingOFsplit ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
+ALTER TABLE FieldingOFsplit ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE FieldingPost ADD PRIMARY KEY (playerID, yearID, teamID, pos, round);
 ALTER TABLE FieldingPost ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
-ALTER TABLE FieldingPost ADD FOREIGN KEY (teamID) REFERENCES teams(teamID);
+ALTER TABLE FieldingPost ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE HallOfFame ADD PRIMARY KEY (playerID, yearID, votedBy);
 DELETE FROM HallOfFame WHERE playerID NOT IN (select playerID FROM Master);
 ALTER TABLE HallOfFame ADD CONSTRAINT fk_HallOfFame_Master FOREIGN KEY (playerID) REFERENCES Master(playerID);
 
 ALTER TABLE HomeGames ADD PRIMARY KEY (`park.KEY`, `team.KEY`, `year.KEY`);
-ALTER TABLE HomeGames ADD FOREIGN KEY (`team.KEY`) REFERENCES teams(teamID);
+ALTER TABLE HomeGames ADD FOREIGN KEY (`team.KEY`) REFERENCES Teams(teamID);
 ALTER TABLE HomeGames ADD FOREIGN KEY (`park.KEY`) REFERENCES parks(`park.KEY`);
 
 ALTER TABLE Managers ADD PRIMARY KEY (playerID, yearID, inseason);
@@ -67,7 +112,7 @@ ALTER TABLE Managers ADD CONSTRAINT fk_Managers_Master FOREIGN KEY (playerID) RE
 
 ALTER TABLE ManagersHalf ADD PRIMARY KEY (playerID, yearID, teamID, half);
 ALTER TABLE ManagersHalf ADD FOREIGN KEY (playerID) REFERENCES Managers(playerID);
-ALTER TABLE ManagersHalf ADD FOREIGN KEY (teamID) REFERENCES teams(teamID);
+ALTER TABLE ManagersHalf ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE Master ADD PRIMARY KEY (playerID)
 
@@ -75,23 +120,23 @@ ALTER TABLE Parks ADD PRIMARY KEY (`park.KEY`);
 
 ALTER TABLE Pitching ADD PRIMARY KEY(playerID, yearID, stint);
 ALTER TABLE Pitching ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
-ALTER TABLE Pitching ADD FOREIGN KEY (teamID) REFERENCES teams(teamID);
+ALTER TABLE Pitching ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE PitchingPost ADD PRIMARY KEY(playerID, yearID, round);
 ALTER TABLE PitchingPost ADD CONSTRAINT fk_PitchingPost_Master FOREIGN KEY (playerID) REFERENCES Master(playerID);
-ALTER TABLE PitchingPost ADD CONSTRAINT fk_PitchingPost_Teams FOREIGN KEY (teamID) REFERENCES teams(teamID);
+ALTER TABLE PitchingPost ADD CONSTRAINT fk_PitchingPost_Teams FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE Salaries ADD PRIMARY KEY(playerID, yearID, teamID);
 DELETE FROM Salaries WHERE playerId NOT IN (select playerId FROM Master);
 DELETE FROM Salaries WHERE teamID NOT IN (select teamID FROM teams);
 ALTER TABLE Salaries ADD FOREIGN KEY (playerID) REFERENCES Master(playerID);
-ALTER TABLE Salaries ADD FOREIGN KEY (teamID) REFERENCES teams(teamID);
+ALTER TABLE Salaries ADD FOREIGN KEY (teamID) REFERENCES Teams(teamID);
 
 ALTER TABLE Schools ADD PRIMARY KEY (schoolID);
 
 ALTER TABLE SeriesPost ADD PRIMARY KEY(teamidwinner,round, yearId);
-ALTER TABLE SeriesPost ADD FOREIGN KEY (teamIDwinner) REFERENCES teams(teamID);
-ALTER TABLE SeriesPost ADD FOREIGN KEY (teamIDloser) REFERENCES teams(teamID);
+ALTER TABLE SeriesPost ADD FOREIGN KEY (teamIDwinner) REFERENCES Teams(teamID);
+ALTER TABLE SeriesPost ADD FOREIGN KEY (teamIDloser) REFERENCES Teams(teamID);
 
 ALTER TABLE TeamsFranchises ADD PRIMARY KEY(franchID);
 
@@ -99,4 +144,4 @@ ALTER TABLE Teams ADD PRIMARY KEY(teamID, yearID);
 ALTER TABLE Teams ADD CONSTRAINT fk_Teams_TeamsFranchises FOREIGN KEY (franchID) REFERENCES teamsfranchises(franchId);
 
 ALTER TABLE TeamShalf ADD PRIMARY KEY(teamId, yearId, half);
-ALTER TABLE TeamShalf ADD FOREIGN KEY(teamID) REFERENCES teams(teamID);
+ALTER TABLE TeamShalf ADD FOREIGN KEY(teamID) REFERENCES Teams(teamID);
